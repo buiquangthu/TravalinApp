fd9b7197c43a461d29c2d910c11e547ff909294c
Thu Bui <bqt05122002@gmail.com>
2025-02-10 13:23:49 +0700

    Add AppRoot component and update dependencies

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+const  AppRoot = () =>{
+
+    return(
+        <View>
+            <Text>Hello 2025</Text>
+        </View>
+    )
+}
+
+export default AppRoot;
